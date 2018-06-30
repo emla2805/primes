99a09ae0552e79e13a0d0b771dfc430b9c631591
Emil <emil.n.larsson@gmail.com>
2018-06-30 19:13:07 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='primes',
+      version='0.1',
+      description='Primality checking',
+      url='https://github.com/emla2805/primes',
+      author='Emil Larsson',
+      author_email='emil.n.larsson@gmail.com',
+      license='MIT',
+      packages=['primes'],
+      zip_safe=False)
