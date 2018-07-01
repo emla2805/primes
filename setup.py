d10833ede8d2a78eaef6422acac8d917f9db7b77
Emil <emil.n.larsson@gmail.com>
2018-07-01 19:26:29 +0200

    Add tox

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os.path import basename, splitext
+from glob import glob
+
 
 setup(name='primes',
       version='0.1',
@@ -7,5 +10,8 @@ setup(name='primes',
       author='Emil Larsson',
       author_email='emil.n.larsson@gmail.com',
       license='MIT',
-      packages=['primes'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      include_package_data=True,
       zip_safe=False)
